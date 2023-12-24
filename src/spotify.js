66a20f1f371d89a6ae4748e661024a412b38ee6e
chrisdten3 <123044283+chrisdten3@users.noreply.github.com>
2023-12-24 02:19:14 -0500

    spotify-update

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from 'axios'; //using axios to make http requests to spotify api
 
 const authEndpoint = "https://accounts.spotify.com/authorize";
 const clientId = "326e311dd5194e27b4e0b0301e4f1adf"; 
-const redirectUri = "http://localhost:3000";
+const redirectUri = "https://chrisdten3.github.io/music-player/";
 const scopes = ["user-library-read", "playlist-read-private", "user-read-currently-playing", "user-read-playback-state", "user-modify-playback-state", "user-top-read", "streaming"];
 
 export const loginEndpoint = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
